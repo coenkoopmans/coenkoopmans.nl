d0b02f7c480e54d034956e2a4b1ea3d9862c8440
Coen Koopmans <info@coenkoopmans.nl>
2018-02-02 00:15:20 +0100

    Cleans up default data; adds intro and menu components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
