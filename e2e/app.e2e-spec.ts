8da584bea6fb14c885d9ca17fa59bf778734bab2
Coen Koopmans <info@coenkoopmans.nl>
2018-02-11 14:27:25 +0100

    Adds initial tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,56 @@
 import { AppPage } from './app.po';
 
-describe('coenkoopmans.nl App', () => {
+describe('Generic app parts', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
+  it('should find all images', function () {
+    page.checkBrokenImages();
+  });
+
+});
+
+describe('Intro component', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+
+  it('should display the page title', () => {
+    const pageTitle = page.getPageTitle();
+    expect(pageTitle).toContain('Ik ben');
+    expect(pageTitle).toContain('Coen');
+    expect(pageTitle).toContain('Koopmans');
+    expect(pageTitle).toContain('.');
+  });
+
+  it('should display the tag line', () => {
+    const tagLineText = page.getTagLine();
+    expect(tagLineText).toEqual('Front-end developer met passie voor schone code.');
+  });
+
+  it('should display the tag line', () => {
+    const tagLineText = page.getTagLine();
+    expect(tagLineText).toEqual('Front-end developer met passie voor schone code.');
+  });
+});
+
+describe('Menu component', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+    page.navigateTo();
+  });
+
+  it('should display the menu button', () => {
+    const menuButton = page.getMenuButton();
+    expect(menuButton.isPresent()).toBeTruthy();
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,30 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  checkBrokenImages() {
+    browser.executeAsyncScript(function (callback) {
+      const imgs = document.getElementsByTagName('img');
+      let loaded = 0;
+      for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].naturalWidth > 0) {
+          loaded = loaded + 1;
+        }
+      }
+      callback(imgs.length - loaded);
+    }).then(function (brokenImagesCount) {
+      expect(brokenImagesCount).toBe(0);
+    });
+  }
+
+  getPageTitle() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getTagLine() {
+    return element(by.css('app-root .tagLine')).getText();
+  }
+
+  getMenuButton() {
+    return element(by.css('app-root app-menu .menuButton'));
   }
 }
