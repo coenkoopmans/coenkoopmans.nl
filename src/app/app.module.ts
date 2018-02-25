8b872eb1e19336b5c810100820cd3737fa3c13d9
Coen Koopmans <info@coenkoopmans.nl>
2018-02-25 18:56:05 +0100

    Adds skills component with heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
-    MenuComponent
+    MenuComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
