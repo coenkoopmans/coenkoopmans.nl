a9c12262e9e694fec8f451022f9f2488fa49d2df
Coen Koopmans <info@coenkoopmans.nl>
2018-02-25 21:14:58 +0100

    Adds basic scrolling functionality to scrollDownButton

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class IntroComponent {
+  public scrollDown() {
+    const element: HTMLElement = document.getElementsByClassName('intro')[0] as HTMLElement;
+    // TODO animate scrolling
+    window.scrollTo(window.scrollX, element.offsetHeight);
   }
 
 }
