6c0e658fd68d9c6047dc83171aafede3f11bfebe
Coen Koopmans <info@coenkoopmans.nl>
2018-03-04 13:51:31 +0100

    Adds about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
     MenuComponent,
-    SkillsComponent
+    SkillsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
