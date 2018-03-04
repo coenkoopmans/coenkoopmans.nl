feef1d958c8e4da1520087eb4c89f85a8f88e375
Coen Koopmans <info@coenkoopmans.nl>
2018-03-04 14:27:23 +0100

    About page content & style improvements

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public age: number;
+  private birthday: Date = new Date('1992-10-02');
 
   constructor() { }
 
   ngOnInit() {
+    this.age = this.calculateAge(this.birthday);
+  }
+
+  private calculateAge(birthday: Date): number {
+    const ageDifference = Date.now() - birthday.getTime();
+    const ageDate = new Date(ageDifference);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
 
 }
