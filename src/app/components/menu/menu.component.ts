ca680917f4a7fb4c48fed07a330cfd38246f5d1d
Coen Koopmans <info@coenkoopmans.nl>
2018-04-09 23:04:44 +0200

    Starts work on menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  public menuImage: string;
 
-  constructor() { }
+  private _menuOpen: boolean;
+  get menuOpen(): boolean { return this._menuOpen; }
+  set menuOpen(menuOpen: boolean) {
+    this._menuOpen = menuOpen;
+    const menuImage: string = menuOpen ? 'menu-button-close' : 'menu-button';
+    this.menuImage = `/assets/ui/${menuImage}.svg`;
+  }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.menuOpen = true;
   }
 
 }
